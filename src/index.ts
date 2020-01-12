51d10bf84c8d59f19991e203388d123ff1e7d963
Ben Tomasini <btomasini@activated.io>
2020-01-11 19:13:11 -0800

    Exporting authentication

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { Authentication } from './Authentication'
 import { PkceSource } from './Pkce';
 import { IParams, ISecure, SecureImpl } from './Secure';
 
 const create = (params: IParams): ISecure => new SecureImpl(params, new PkceSource());
 
-export { ISecure, IParams, create };
+export { Authentication, ISecure, IParams, create };
