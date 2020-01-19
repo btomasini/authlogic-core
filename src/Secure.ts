81e061abf3926dcfd74bc2a4e410ab7402f05212
Ben Tomasini <btomasini@activated.io>
2020-01-18 19:07:55 -0800

    0.5.2

--- src/Secure.ts
+++ src/Secure.ts
@@ -139,17 +139,11 @@ class SecureImpl implements ISecure {
     const p = this.params!;
     const redirectUri = window.location.href;
     window.location.assign(
-      `${this.params!.issuer}/authorize?client_id=${
-      p.clientId
-      }&redirect_uri=${
-      encodeURIComponent(redirectUri)
-      }&state=${
-      storage.state
-      }&nonce=${storage.nonce}&response_type=code&scope=${
-      encodeURIComponent(p.scope)
-      }&code_challenge=${
-      encodeURIComponent(storage.pkce.challenge)
-      }`,
+      `${this.params!.issuer}/authorize?client_id=${p.clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${
+        storage.state
+      }&nonce=${storage.nonce}&response_type=code&scope=${encodeURIComponent(
+        p.scope,
+      )}&code_challenge=${encodeURIComponent(storage.pkce.challenge)}`,
     );
   }
 
